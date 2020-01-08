55941fd27500e228db68599a4914c774a5c53c98
Víctor <vbergaes@mercadona.es>
2020-01-08 15:12:45 +0100

    Bump swift verstion to 5.1

--- Package.swift
+++ Package.swift
@@ -5,16 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Presentr",
-    platforms: [
-        .iOS(.v11)
-    ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "Presentr", targets: ["Presentr"]),
-    ],
+    platforms: [.iOS(.v11)],
+    products: [.library(name: "Presentr", targets: ["Presentr"])],
     dependencies: [],
-    targets: [
-        .target(name: "Presentr", path: "Presentr"),
-        .testTarget(name: "PresentrTests", dependencies: ["Presentr"], path: "PresentrTests"),
-    ]
+    targets: [.target(name: "Presentr", path: "Presentr")]
 )
